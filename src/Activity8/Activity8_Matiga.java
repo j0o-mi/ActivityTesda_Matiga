a64a439d2fc043febec69e17119535b30500710e
j0o-mi <johnmichaelmatiga22@gmail.com>
2026-07-06 21:23:33 +0800

    code complete

--- src/Activity8/Activity8_Matiga.java
+++ src/Activity8/Activity8_Matiga.java
@@ -1,7 +1,54 @@
 package Activity8;
+import java.util.Scanner;
 
 public class Activity8_Matiga {
+
+    static class Student {
+         String name;
+         int age;
+         String course;
+
+         Student() {
+             name = "Unknown";
+             age = 0;
+             course = "Unknown";
+         }
+
+        Student (String name, int age, String course) {
+            this.name = name;
+            this.age = age;
+            this.course = course;
+        }
+
+        void display (int studentCount) {
+            System.out.println("\nStudent " + studentCount + " Details:");
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Course: " + course);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        Student student1 = new Student();
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Student Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Student Course: ");
+        String course = sc.nextLine();
+
+        Student student2 = new Student(name, age, course);
+
+        student1.display(1);
+        student2.display(2);
+
+        sc.close();
+
     }
 }
